ca779cb28803c9ea0bb276c053d2de448e9b162e
rychouwei <rychouwei@tencent.com>
2026-08-07 18:48:36 +0800

    release: TRTC Web SDK 5.19.1

--- SDK/plugins/video-effect/face-detection/face-detection.esm.d.ts
+++ SDK/plugins/video-effect/face-detection/face-detection.esm.d.ts
@@ -1,14 +1,22 @@
 export interface FaceDetectionOptions {
+  /** 人脸检测状态变化的回调函数 */
   onFaceDetectionStateChanged: (hasFace: boolean) => void;
+  /** 两次推理执行的最小间隔，单位 ms，默认 300，范围 [100, 5000]。该值同时控制推理频率和回调节流 */
   detectionInterval?: number;
+  /** 人脸检测最小置信度，默认 0.8，范围 [0, 1] */
   minConfidence?: number;
+  /** 人脸消失后等待多久才触发状态变为 false 的回调，单位 ms，默认 1000，范围 [0, 5000] */
   missingTimeout?: number;
 }
 
 export interface UpdateFaceDetectionOptions {
+  /** 人脸检测状态变化的回调函数 */
   onFaceDetectionStateChanged?: (hasFace: boolean) => void;
+  /** 两次推理执行的最小间隔，单位 ms，默认 300，范围 [100, 5000] */
   detectionInterval?: number;
+  /** 人脸检测最小置信度，默认 0.8，范围 [0, 1] */
   minConfidence?: number;
+  /** 人脸消失后等待多久才触发状态变为 false 的回调，单位 ms，默认 1000，范围 [0, 5000] */
   missingTimeout?: number;
 }
 
